c314ac7cb19c8d5fea20f8631c1878bd105fae92
Vittorio Polverino <vittorio.polverino@brumbrum.it>
2022-10-12 13:03:11 +0200

    pandas import and expectations run with dataframe args

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,10 @@
+import pandas as pd
 import src.expectations_example as expectations
 
 
 def handler(event, context):
     print("hello World, i'm a containerized Lambda function")
     event_payload = event["payload"]
-    expectations.run(data=event_payload)
+    expectations.run(dataframe=pd.DataFrame(event_payload))
 
     return {"statusCode": 200}
